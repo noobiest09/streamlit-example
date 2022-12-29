d5de786eb4deda8872113a392d4d6fe5a5a650be
samsylim <samuelson.sylim@gmail.com>
2022-12-29 17:16:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Caching methods for memory management
 
-@st.cache(hash_funcs={'xgboost.XGBClassifier': id})
+@st.cache(allow_output_mutation=True)
 def load_model(file_path):
     
     # Load Models

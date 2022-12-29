5a921c91214d259d6ea210d9edc2e78beef149b7
samsylim <samuelson.sylim@gmail.com>
2022-12-29 15:52:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,85 +89,91 @@ def process_data(df):
     return X, X_log
 
 
+def make_predictions():
+        model_v3 = XGBClassifier()
+        model_v2_1 = XGBClassifier()
+        model_v2_2 = XGBClassifier()
+        model_v3.load_model("v3_model.json")
+        model_v2_1.load_model("xgb_model_v2_1.json")
+        model_v2_2.load_model("xgb_model_v2_2.json")
+
+        global v2_data, v3_data
+        global prediction_v2_1, prediction_v2_2, prediction_v2, prediction_v3
+        global confidence_v2_1, confidence_v2_2, confidence_v3
+
+        v2_data, v3_data = process_data(df)
+        v2_data = v2_data[features_list]
+        v3_data = v3_data[features_list]
+        
+        # V2 Output
+        prediction_v2_1 = model_v2_1.predict(v2_data)[0]
+        prediction_v2_2 = model_v2_2.predict(v2_data)[0]
+        prediction_v2 = (1 - prediction_v2_1) * prediction_v2_2
+        confidence_v2_1 = model_v2_1.predict_proba(v2_data)[0].max()
+        confidence_v2_2 = model_v2_2.predict_proba(v2_data)[0].max()
+        prediction_v3 = model_v3.predict(v3_data)[0]
+        confidence_v3 = model_v3.predict_proba(v3_data)[0].max()
+        
+        # V2 Output
+        st.success('V2 Model:' +
+                   '  \n--------------' +
+                   '  \nPrediction: ' + v2_predict_dict[prediction_v2] +
+                   '  \nBinary Confidence: ' + str(round(confidence_v2_1*100, 1)) +
+                   '%' + ('' if prediction_v2_1 else '  \nBinned Confidence: ' +
+                          str(round(confidence_v2_2*100, 1)) + '%'))
+        
+        # V3 Output
+        st.success('V3 Model:' +
+                   '  \n--------------'
+                   '  \nPrediction: ' + v3_predict_dict[prediction_v3] +
+                   '  \nConfidence: ' + str(round(confidence_v3*100, 1)) + '%')
+
+
 # Input Data
 df = pd.DataFrame()
 
-c1, c2 = st.columns(2)
-
-with c1:
-    df.loc[0, 'Gender'] = st.selectbox(label='Gender',
-                                       options=['Male', 'Female'],
-                                       index=1
-                                       )
-    df.loc[0, 'Job Class'] = st.selectbox(label='Job Class',
-                                          options=job_classes)
-    df.loc[0, 'Employment Status'] = st.selectbox(label='Employment Status',
-                                                  options=employment_stats
-                                                  )
+with st.form("my_form"):
+    c1, c2 = st.columns(2)
     
-with c2:
-    df.loc[0, 'Salary'] = st.number_input(label='Salary',
-                                          min_value=0.00,
-                                          value=25000.00,
-                                          step=1e-2)
-    df.loc[0, 'Tenure (years)'] = st.number_input(label='Tenure',
-                                          min_value=0.00,
-                                          max_value=100.00,
-                                          value=5.00, step=1e-2)
-    df.loc[0, 'Age'] = st.number_input(label='Age',
-                                       min_value=0,
-                                       max_value=100,
-                                       value=30)
-    
-
-# Load Models
-model_v3 = XGBClassifier()
-model_v2_1 = XGBClassifier()
-model_v2_2 = XGBClassifier()
-model_v3.load_model("v3_model.json")
-model_v2_1.load_model("xgb_model_v2_1.json")
-model_v2_2.load_model("xgb_model_v2_2.json")
-
-# Button
-
-predict_button = st.button('Predict')
-
-if st.session_state.get('button') != True:
-    st.session_state['button'] = predict_button
-
-if st.session_state['button'] == True:
-    v2_data, v3_data = process_data(df)
-    v2_data = v2_data[features_list]
-    v3_data = v3_data[features_list]
-    
-    # V2 Output
-    prediction_v2_1 = model_v2_1.predict(v2_data)[0]
-    prediction_v2_2 = model_v2_2.predict(v2_data)[0]
-    prediction_v2 = (1 - prediction_v2_1) * prediction_v2_2
-    confidence_v2_1 = model_v2_1.predict_proba(v2_data)[0].max()
-    confidence_v2_2 = model_v2_2.predict_proba(v2_data)[0].max()
-    st.success('V2 Model:' +
-               '  \n--------------' +
-               '  \nPrediction: ' + v2_predict_dict[prediction_v2] +
-               '  \nBinary Confidence: ' + str(round(confidence_v2_1*100, 1)) +
-               '%' + ('' if prediction_v2_1 else '  \nBinned Confidence: ' +
-                      str(round(confidence_v2_2*100, 1)) + '%'))
-    
-    # V3 Output
-    prediction_v3 = model_v3.predict(v3_data)[0]
-    confidence_v3 = model_v3.predict_proba(v3_data)[0].max()
-    st.success('V3 Model:' +
-               '  \n--------------'
-               '  \nPrediction: ' + v3_predict_dict[prediction_v3] +
-               '  \nConfidence: ' + str(round(confidence_v3*100, 1)) + '%')
-
-    
-    if st.button('Show SHAP Plots'):
+    with c1:
+        df.loc[0, 'Gender'] = st.selectbox(label='Gender',
+                                           options=['Male', 'Female'],
+                                           index=1
+                                           )
+        df.loc[0, 'Job Class'] = st.selectbox(label='Job Class',
+                                              options=job_classes)
+        df.loc[0, 'Employment Status'] = st.selectbox(label='Employment Status',
+                                                      options=employment_stats
+                                                      )
         
+    with c2:
+        df.loc[0, 'Salary'] = st.number_input(label='Salary',
+                                              min_value=0.00,
+                                              value=25000.00,
+                                              step=1e-2)
+        df.loc[0, 'Tenure (years)'] = st.number_input(label='Tenure',
+                                              min_value=0.00,
+                                              max_value=100.00,
+                                              value=5.00, step=1e-2)
+        df.loc[0, 'Age'] = st.number_input(label='Age',
+                                           min_value=0,
+                                           max_value=100,
+                                           value=30)
+    
+    submitted1 = st.form_submit_button('Predict')
+    submitted2 = st.form_submit_button('Predict & Show SHAP Plots')
+    
+    if submitted1: # Predict Only
+        # Make predictions
+        make_predictions()
+        
+        
+    if submitted2: # Predict & Plot SHAP
+        
+        # Make predictions
+        make_predictions()
+
         # Explainer values
-        # explainer_v2_1 = shap.Explainer(model_v2_1)
-        # explainer_v2_2 = shap.Explainer(model_v2_2)
-        # explainer_v3 = shap.Explainer(model_v3)
         with lzma.open('explainer_v2_1.xz', 'rb') as f:
             explainer_v2_1 = pickle.load(f)
         with lzma.open('explainer_v2_2.xz', 'rb') as f:
@@ -193,10 +199,10 @@ if st.session_state['button'] == True:
                 figsize=(22, 4)
             )
             st.pyplot(bbox_inches='tight', dpi=300, pad_inches=0)
-            st.caption("Features in :red[pink] push the prediction towards the _" + 
-                       v2_binary_dict[prediction_v2_1] + "_ prediction. :blue[Blue bars] drag the "
-                       "prediction away from the ___" + v2_predict_dict[prediction_v2_2]
-                       + "___ assignment."
+            st.caption("Features in pink push the prediction towards the _" + 
+                       v2_binary_dict[prediction_v2_1] + "_ prediction. Blue bars drag the "
+                       "prediction away from the _" + v2_predict_dict[prediction_v2_2]
+                       + "_ assignment."
                        )
             
         else:
@@ -210,8 +216,8 @@ if st.session_state['button'] == True:
             )
             st.pyplot(bbox_inches='tight', dpi=300, pad_inches=0)
             st.caption("Features in :red[pink] push the prediction towards a " 
-                       "___Not Resigned___ prediction. :blue[Blue bars] push the prediction"
-                       " towards ___Resigned___."
+                       "_Not Resigned_ prediction. :blue[Blue bars] push the prediction"
+                       " towards _Resigned_."
                        )
             
     
